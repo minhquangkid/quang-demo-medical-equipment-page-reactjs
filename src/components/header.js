26c528bf00dfdcfdef2696ce03068ab0626dc736
minhquang <you@example.com>
2024-01-07 17:00:43 +0700

    install icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,20 @@
 import React from "react";
 import logo from "../asset/image/logo_sendora_new-v2023.png";
 import "./header.css";
+import { LuPhone } from "react-icons/lu";
+import { FaRegHeart } from "react-icons/fa6";
+import { PiShoppingCart } from "react-icons/pi";
 
 const Header = () => {
   return (
     <React.Fragment>
       <div className="content">
         <div className="frame">
-          <div className="above">
-            <div className="logo">
+          <div className="above d-flex">
+            <div className="logo flex-grow-1">
               <img src={logo} className="logo-header" alt="" />
             </div>
-            <div className="search-frame">
+            <div className="search-frame d-flex align-items-center">
               <select>
                 <option>Sản phẩm</option>
                 <option>Bàn ghế</option>
@@ -23,9 +26,24 @@ const Header = () => {
               />
               <button>Search</button>
             </div>
-            <div className="call-block">Gọi ngay</div>
-            <div className="favorite-block">Trái tim</div>
-            <div className="cart-block">Giỏ hàng</div>
+            <div className="call-block d-flex align-items-center">
+              <LuPhone style={{ fontSize: 30, margin: 10 }} />
+              <div>
+                <p>Gọi ngay</p>
+                <p style={{ fontWeight: 700 }}>0934267612</p>
+              </div>
+            </div>
+            <div className="favorite-block d-flex align-items-center">
+              <div className="inside-favorite">
+                <FaRegHeart className="icon-block" />
+                <div className="d-flex algin-items-center favorite-count">
+                  0
+                </div>
+              </div>
+            </div>
+            <div className="cart-block">
+              <PiShoppingCart />
+            </div>
             <div className="register-block">Đăng ký</div>
           </div>
           <hr></hr>
