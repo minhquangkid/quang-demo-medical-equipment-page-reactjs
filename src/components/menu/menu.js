a46084f575e39a5b3169ed0538ed0bd9a92079cb
quang <minhquangkid@gmail.com>
2024-03-17 15:55:22 +0700

    add banner

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import categories from "../../data/category.json";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import banner1 from "../../asset/image/banner-giuong-sendora.jpg";
+import banner2 from "../../asset/image/xe-day-spa-giam-gia-tha-ga.jpg";
+import banner3 from "../../asset/image/z5224800525903_785f7c5bf09a8f3b1fe76c61152bc0da.jpg";
 import "./menu.css";
 
 const Menu = () => {
@@ -25,7 +28,58 @@ const Menu = () => {
           <div className="col-3">
             <div className="border-menu">{listCategory}</div>
           </div>
-          <div className="col-9">sdsadd</div>
+          <div className="col-9 mt-2">
+            {/* <img src={banner} alt="banner" className="banner-img" /> */}
+            <div id="demo" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#demo"
+                  data-bs-slide-to="0"
+                  class="active"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#demo"
+                  data-bs-slide-to="1"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#demo"
+                  data-bs-slide-to="2"
+                ></button>
+              </div>
+
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src={banner1} alt="Los Angeles" class="d-block w-100" />
+                </div>
+                <div class="carousel-item">
+                  <img src={banner2} alt="Chicago" class="d-block w-100" />
+                </div>
+                <div class="carousel-item">
+                  <img src={banner3} alt="New York" class="d-block w-100" />
+                </div>
+              </div>
+
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#demo"
+                data-bs-slide="prev"
+              >
+                <span class="carousel-control-prev-icon"></span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#demo"
+                data-bs-slide="next"
+              >
+                <span class="carousel-control-next-icon"></span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
